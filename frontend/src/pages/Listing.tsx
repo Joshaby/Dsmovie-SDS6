7cf0caaf217073183b9387a49114be2eba5e520b
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2022-01-11 12:46:48 -0300

    MovieCard

--- frontend/src/pages/Listing.tsx
+++ frontend/src/pages/Listing.tsx
@@ -1,8 +1,30 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 const Listing = () => {
     return (
-        <Pagination />
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
