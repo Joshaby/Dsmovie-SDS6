176371308e41ffefeaa6b86471c7570950f96f20
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2022-01-14 13:29:32 -0300

    Props

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -1,16 +1,13 @@
 import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
 import MovieScore from "./MovieScore";
 import './styles.css'
 
-const MovieCard = () => {
+type Props = {
+    movie: Movie;
+}
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+const MovieCard = ({ movie } : Props) => {
 
     return (
         <div>
