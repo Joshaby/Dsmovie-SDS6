eef93e1daee1784ab22561e357cfe4158356aa1e
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2022-01-14 12:10:58 -0300

    First request

---
+++ frontend/src/types/movie.ts
@@ -0,0 +1,18 @@
+export type Movie = {
+    id: number;
+    title: string;
+    score: number;
+    count: number;
+    image: string;
+}
+
+export type MoviePage = {
+    content: Movie[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size: number;
+    first: boolean;
+    numberOfElements: number;
+    empty: boolean;
+}
